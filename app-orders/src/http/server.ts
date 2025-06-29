9982c48cb4a6a5d5900356693c448f8dbf8b0df2
juanpablow <juan.santana.macedo10@outlook.com>
2025-06-28 22:01:19 -0300

    [chore] create and dispatch order

--- app-orders/src/http/server.ts
+++ app-orders/src/http/server.ts
@@ -1,16 +1,23 @@
 import { fastify } from "fastify";
 // import { fastifyCors } from "@fastify/cors"
+import fastifyCors from "@fastify/cors";
+import { randomUUID } from "crypto";
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import { z } from "zod";
+import { dispatchOrderCreated } from "../broker/messages/order-created.ts";
+import { db } from "../db/client.ts";
+import { schema } from "../db/schema/index.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifyCors, { origin: "*" });
 
 app.get("/health", () => {
   return "OK";
@@ -21,14 +28,30 @@ app.post(
   {
     schema: {
       body: z.object({
-        amount: z.number(),
+        amount: z.coerce.number(),
       }),
     },
   },
-  (request, reply) => {
+  async (request, reply) => {
     const { amount } = request.body;
 
-    console.log("Creating an order with amount", amount);
+    const orderId = randomUUID();
+
+    dispatchOrderCreated({
+      orderId,
+      amount,
+      customer: {
+        id: "e5d26176-0b50-47d3-aa2b-3b1a81963c16",
+      },
+    });
+
+    await db.insert(schema.orders).values({
+      id: orderId,
+      customerId: "e5d26176-0b50-47d3-aa2b-3b1a81963c16",
+      amount,
+    });
+
+    return reply.status(201).send();
   }
 );
 
