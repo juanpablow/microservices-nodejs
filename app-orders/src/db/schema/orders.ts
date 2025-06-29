48f24d0c439101f5dd90bfc4d8aa51601f389a26
juanpablow <juan.santana.macedo10@outlook.com>
2025-06-28 21:53:35 -0300

    [feat] create orders table

--- app-orders/src/db/schema/orders.ts
+++ app-orders/src/db/schema/orders.ts
@@ -1,10 +1,17 @@
 import { integer, pgEnum, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { customers } from "./customers.ts";
 
-const orderStatusEnum = pgEnum("order_status", ["pending", "paid", "canceled"]);
+export const orderStatusEnum = pgEnum("order_status", [
+  "pending",
+  "paid",
+  "canceled",
+]);
 
 export const orders = pgTable("orders", {
   id: text().primaryKey(),
-  customerId: text().notNull(),
+  customerId: text()
+    .notNull()
+    .references(() => customers.id),
   amount: integer().notNull(),
   status: orderStatusEnum().notNull().default("pending"),
   createdAt: timestamp().defaultNow().notNull(),
