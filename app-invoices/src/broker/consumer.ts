3de3113a52597501cfc36d7139af90e9a6e316d5
juanpablow <juan.santana.macedo10@outlook.com>
2025-07-01 19:43:44 -0300

    [feat] add app-invoices stucture

---
+++ app-invoices/src/broker/consumer.ts
@@ -0,0 +1,19 @@
+import { orders } from "./channels/orders.ts";
+
+orders.consume(
+  "orders",
+  async (message) => {
+    if (!message) {
+      return null;
+    }
+
+    console.log(message?.content.toString());
+
+    orders.ack(message);
+  },
+  {
+    noAck: false,
+  }
+);
+
+// acknowledge => reconhecer
